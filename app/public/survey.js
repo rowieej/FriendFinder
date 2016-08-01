e8e96b729ac9e36de99a5c57e8f7c9e93a4a8a99
rowieej <rowena.jaico@gmail.com>
2016-07-31 23:45:50 -0400

    fixed function in survey.js and shortened some code

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,45 +1,53 @@
 var newFriend;
 
-function User(name, pic, answers){
-  this.name = name;
-  this.picture = pic;
-  this.scores = answers;
+// function User(name, pic, answers){
+//   this.name = name;
+//   this.picture = pic;
+//   this.scores = answers;
 
-};
+// };
 
+$('#submit-survey').on('click',  function(){
 
+  var newFriend = {
+        name: $("#name").val(),
+        pic: $("#image-link").val(),
+        answers: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val(),]
+        }
 
-var getInfo = function(){
-  var name = $('#name').val().trim();
-  var pic = $('#image-link').val().trim();
-  var answers = [];
+  // var getInfo = function(){
+  //   var name = $('#name').val().trim();
+  //   var pic = $('#image-link').val().trim();
+  //   var answers = [];
 
-  answers.push($('#q1').val().trim());
-  answers.push($('#q2').val().trim());
-  answers.push($('#q3').val().trim());
-  answers.push($('#q4').val().trim());
-  answers.push($('#q5').val().trim());
-  answers.push($('#q6').val().trim());
-  answers.push($('#q7').val().trim());
-  answers.push($('#q8').val().trim());
-  answers.push($('#q9').val().trim());
-  answers.push($('#q10').val().trim());
+  //   answers.push($('#q1').val());
+  //   answers.push($('#q2').val());
+  //   answers.push($('#q3').val());
+  //   answers.push($('#q4').val());
+  //   answers.push($('#q5').val());
+  //   answers.push($('#q6').val());
+  //   answers.push($('#q7').val());
+  //   answers.push($('#q8').val());
+  //   answers.push($('#q9').val());
+  //   answers.push($('#q10').val());
 
-
-
-   newFriend = new User(name, pic, answers);
-
-   
    var currentUrl = window.location.origin;
 
-   $.post(currentUrl + '/api/friends', newFriend, function(data){
+     $.post(currentUrl + '/api/friends', newFriend, function(data){
+            $("#matchName").text(data.name);
+            $('#matchImg').attr("src", data.pic);
 
-    console.log(data);
+            
+            $("#resultsModal").modal('toggle');
 
-   });
+          });
+      
+        
+        return false;
 
-   return false;
-};
+     
+  });
+
+    
 
 
-$('#submit-survey').on('click',  getInfo);
